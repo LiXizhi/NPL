ecedf698600d0347906e678a20bafc9a135a6366
Zhiyuan <zyguo1230@126.com>
2017-01-15 18:32:15 +0800

    Implemented feature format

--- VsNPL/NPLLanguageService/NPLFormatHelper.cs
+++ VsNPL/NPLLanguageService/NPLFormatHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.Package;
 using Microsoft.VisualStudio.TextManager.Interop;
 using System.Collections.Generic;
@@ -16,9 +17,39 @@ namespace ParaEngine.Tools.Lua
         /// <param name="span"></param>
         /// <param name="tabSize"></param>
         /// <returns></returns>
-        internal static List<EditSpan> ReformatCode(IVsTextLines pBuffer, TextSpan span, int tabSize)
+        internal static List<EditSpan> ReformatCode(IVsTextLines pBuffer, int[] indents, TextSpan span, int tabSize)
         {
-            return null;
+            List<EditSpan> changeList = new List<EditSpan>();
+            string line = "";
+            for (int i = span.iStartLine; i <= span.iEndLine; ++i)
+            {
+                TextSpan editTextSpan = new TextSpan();
+                editTextSpan.iStartLine = i;
+                editTextSpan.iEndLine = i;
+                editTextSpan.iStartIndex = 0;
+
+                int startIndex = 0;
+                int endIndex = 0;
+                pBuffer.GetLengthOfLine(i, out endIndex);
+                editTextSpan.iEndIndex = endIndex;
+                pBuffer.GetLineText(i, startIndex, i, endIndex, out line);
+
+                string pat = @"^[' '\t]*";
+                Regex regex = new Regex(pat);
+                Match m = regex.Match(line);
+                if (m.Success)
+                {
+                    Capture c = m.Groups[0].Captures[0];
+                    line = line.Substring(c.Index + c.Length);
+                }
+
+                for (int j = 0; j < tabSize * indents[i]; ++j)
+                    line = " " + line;
+
+                changeList.Add(new EditSpan(editTextSpan, line));
+            }
+
+            return changeList;
         }
     }
 }
