e1d6d1ff73c7c3a685f7885984ed41c5ad71f3b7
Zhiyuan <zyguo1230@126.com>
2017-01-24 08:48:07 +0800

    sync

--- VsNPL/NPLLanguageService/LuaSource.cs
+++ VsNPL/NPLLanguageService/LuaSource.cs
@@ -4,7 +4,10 @@ using Microsoft.VisualStudio.TextManager.Interop;
 using ParaEngine.Tools.Lua.Parser;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Source = ParaEngine.Tools.Lua.Parser.Source;
+using LuaParser = ParaEngine.Tools.Lua.Parser.Parser;
+using ParaEngine.Tools.Lua.AST;
 
 namespace ParaEngine.Tools.Lua
 {
@@ -13,7 +16,8 @@ namespace ParaEngine.Tools.Lua
 	/// </summary>
     public class LuaSource : Source
     {
-		/// <summary>
+        private int[] indents;
+        /// <summary>
 		/// Initializes a new instance of the <see cref="LuaSource"/> class.
 		/// </summary>
 		/// <param name="service">The service.</param>
@@ -22,6 +26,7 @@ namespace ParaEngine.Tools.Lua
         public LuaSource(BaseLanguageService service, IVsTextLines textLines, Colorizer colorizer)
 			: base(service, textLines, colorizer)
 		{
+            //IndentInitialize();
 		}
 
         public override void OnIdle(bool periodic)
@@ -30,6 +35,10 @@ namespace ParaEngine.Tools.Lua
             // We're not yet doing an explicit first parse and the MPF assumes that we are. 
             if (this.LastParseTime == Int32.MaxValue)
                 this.LastParseTime = this.LanguageService.Preferences.CodeSenseDelay;
+
+            // Zhiyuan: update the indents when content of text changed
+            //if (IsDirty)
+                //IndentInitialize();
 
             base.OnIdle(periodic);
         }
@@ -66,14 +75,20 @@ namespace ParaEngine.Tools.Lua
             }
         }
 
+        private void IndentInitialize()
+        {
+            indents = new int[GetLineCount()];
+            Chunk chunk = ParseSource(GetText());
+            GetIndents(chunk, indents);
+        }
+
         private void DoFormatting(EditArray mgr, TextSpan span)
         {
-            // Make sure there is one space after every comma unless followed
-            // by a tab or comma is at end of line.
+            IndentInitialize();
             IVsTextLines pBuffer = GetTextLines();
             if (pBuffer != null)
             {
-                List<EditSpan> changeList = NPLFormatHelper.ReformatCode(pBuffer, span, LanguageService.GetLanguagePreferences().TabSize);
+                List<EditSpan> changeList = NPLFormatHelper.ReformatCode(pBuffer, indents, span, LanguageService.GetLanguagePreferences().TabSize);
                 if (changeList != null)
                 {
                     foreach (EditSpan editSpan in changeList)
@@ -81,8 +96,100 @@ namespace ParaEngine.Tools.Lua
                         // Add edit operation
                         mgr.Add(editSpan);
                     }
+                    mgr.ApplyEdits();
                 }
             }
+        }
+
+        private Chunk ParseSource(string source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            //Create a parser for the request
+            LuaParser parser = CreateParser();
+
+            // Set the source
+            ((LuaScanner)parser.scanner).SetSource(source, 0);
+
+            // Trigger the parse (hidden region and errors will be added to the AuthoringSink)
+            parser.Parse();
+
+            return parser.Chunk;
+        }
+
+        private LuaParser CreateParser()
+        {
+            var handler = new ParaEngine.Tools.Lua.Parser.ErrorHandler();
+
+            LuaScanner scanner = new LuaScanner();
+            LuaParser parser = new LuaParser();
+
+            scanner.Handler = handler;
+
+            parser.scanner = scanner;
+
+            parser.Request = null;
+
+            return parser;
+        }
+
+        // get indentations for each line
+        private void GetIndents(Chunk chunk, int[] indents)
+        {
+            // start with -1
+            //Trace.Write(chunk.GetStringRepresentation());
+            int currentIndent = -1;
+
+            for (int i = 0; i < chunk.Location.sLin - 1; ++i)
+                indents[i] = 0;
+            for (int i = chunk.Location.sLin; i < chunk.Location.eLin; ++i)
+                indents[i] = -1;
+
+            SetIndent(chunk, currentIndent, indents);
+
+            // dealed with unset lines, normally comments or blank lines
+            for (int i = indents.Length - 1; i >= 0; --i)
+            {
+                if (i == indents.Length - 1)
+                    indents[i] = indents[i] == -1 ? 0 : indents[i];
+                else
+                    indents[i] = indents[i] == -1 ? indents[i + 1] : indents[i];
+            }
+        }
+
+        // recursively set indentation for each line in Ast tree
+        private void SetIndent(Node node, int currentIndent, int[] indents)
+        {
+            int increment = 0;
+            if (node is Block)
+            {
+                increment = 1;
+                if (indents[node.Location.sLin - 1] == -1)
+                    indents[node.Location.sLin - 1] = currentIndent;
+                //if (indents[node.Location.eLin - 1] == -1)
+                //    indents[node.Location.eLin - 1] = currentIndent;
+            }
+            else if (node is ThenBlock || node is ElseIfBlock)
+            {
+                // nothing 
+            }
+            else
+            {
+                if (indents[node.Location.sLin - 1] == -1)
+                    indents[node.Location.sLin - 1] = currentIndent;
+                if (indents[node.Location.eLin - 1] == -1)
+                    indents[node.Location.eLin - 1] = currentIndent;
+            }
+
+            foreach (var childNode in node.GetChildNodes())
+            {
+                if (childNode != null)
+                    SetIndent(childNode, currentIndent + increment, indents);
+            }
+
+            if (node.Next != null)
+                SetIndent(node.Next, currentIndent, indents);
         }
     }
 }
